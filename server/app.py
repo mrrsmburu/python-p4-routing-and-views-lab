a84ed7bdd808dce04d95d1b178d5088231954f36
mrrsmburu <mrrsmburu@gmail.com>
2024-07-10 13:19:48 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -6,20 +6,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Python Operations with Flask Routing and Views<h1>'
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<parameter>')
+@app.route('/print/<string:parameter>')
 def print_string(parameter):
     print(parameter)
-    return f'<h2>{parameter}</h2>'
+    return parameter
 
 @app.route('/count/<int:parameter>')
 def count(parameter):
-    count_list = '<br>'.join(str(i) for i in range(parameter + 1))
-    return f'<h2>Counting up to {parameter}:</h2><p>{count_list}</p>'
+    numbers = '\n'.join(str(i) for i in range(parameter)) + '\n'
+    return numbers
 
-@app.route('/math/<int:num1>/<operation>/<int:num2>')
+@app.route('/math/<path:num1>/<path:operation>/<path:num2>')
 def math(num1, operation, num2):
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        return 'Invalid numbers'
+
     if operation == '+':
         result = num1 + num2
     elif operation == '-':
@@ -27,12 +33,25 @@ def math(num1, operation, num2):
     elif operation == '*':
         result = num1 * num2
     elif operation == 'div':
-        result = num1 / num2 if num2 != 0 else 'undefined (division by zero)'
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return 'Error: Division by zero'
     elif operation == '%':
-        result = num1 % num2
+        if num2 != 0:
+            result = num1 % num2
+        else:
+            return 'Error: Division by zero'
     else:
-        result = 'Invalid operation'
-    return f'<h2>{num1} {operation} {num2} = {result}</h2>'
+        return 'Invalid operation'
+    
+    if operation == 'div' or not result.is_integer():
+        result = float(result)
+    else:
+        result = int(result)
+    
+    return str(result)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
